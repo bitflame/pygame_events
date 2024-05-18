b7903507e436a5ab19392c872a6ef6e1271989b0
bitflame <monazzam@protonmail.com>
2024-05-18 19:33:34 -0400

    initial commit - Exercise 12-5 prints the id for keys that are pressed.

---
+++ exercise_12_5.py
@@ -0,0 +1,17 @@
+import pygame
+
+#pygame setup 
+pygame.init()
+screen = pygame.display.set_mode((1200, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            print(event.key)
+            
+    screen.fill("purple")
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
